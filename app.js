fa4ef499d9112ba4b89055c2db5cdd331842b5c4
ali778866 <mdali.ali76@gmail.com>
2023-07-28 20:38:41 +0530

    body-parser added and two input tag was console

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
 const express = require('express');
-
+const bodyParser = require('body-parser');
 const app = express();
 
-app.use((req, res, next) => {
-    console.log('in the first Middleware!');
-    next();
+app.use(bodyParser.urlencoded({extended: false}));
+
+app.use('/add-product', (req, res, next) => {
+    res.send('<body><form action="/product" method="POST"><input type="text" name="title" /><input type="number" name="size" /><button type="submit">Send</button></form></body>');
 });
 
-app.use((req, res, next) => {
-    console.log('In the second Middleware');
+app.use('/product', (req, res, next) =>{
+    console.log(req.body);
+    res.redirect('/');
+})
+
+app.use('/', (req, res, next) => {
     res.send('<h1>Hello From Express!</h1>');
 });
 
