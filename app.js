c178be9141ca7de22fb07e3949f60290e3325383
ali778866 <mdali.ali76@gmail.com>
2023-07-30 08:36:14 +0530

    Routers and Filters added

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+
 const app = express();
+
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<body><form action="/product" method="POST"><input type="text" name="title" /><input type="number" name="size" /><button type="submit">Send</button></form></body>');
-});
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
 
-app.use('/product', (req, res, next) =>{
-    console.log(req.body);
-    res.redirect('/');
+app.use((req, res, next) => {
+    res.status(404).send('<h1>404 Error! Page not found...</h1>');
 })
-
-app.use('/', (req, res, next) => {
-    res.send('<h1>Hello From Express!</h1>');
-});
 
 app.listen(4000);
